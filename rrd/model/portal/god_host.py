753a19563e7aa9c28192429c229b93a54e17894f
王一琦 <wangyiqi@gridsum.com>
2017-12-15 11:31:32 +0800

    agent status detection module

---
+++ rrd/model/portal/god_host.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+# Copyright 2017 Gridsum, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+__author__ = 'Dake Wang'
+from .bean import Bean
+from rrd.store import db
+
+class God_host(Bean):
+    _tbl = "god"
+    _cols = "id, product, hostname, ips"
+
+    def __init__(self, _id, product, hostname, ips):
+        self.id = _id
+        self.product = product
+        self.hostname = hostname
+        self.ips = ips
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "product": self.product,
+            "hostname": self.hostname,
+            "ips": self.ips,
+        }
+
+    @classmethod
+    def product_host_dict(cls, product):
+        sql = "SELECT hostname,ips FROM god WHERE product = '" + product + "'"
+        rows = db.query_all(sql)
+        ret = {}
+        if rows:
+            for row in rows:
+                ret[row[0]] = row[1]
+
+    @classmethod
+    def all_product(cls):
+        ret = []
+        sql = "SELECT distinct product FROM god"
+        rows = db.query_all(sql)
+        if rows:
+            for row in rows:
+                ret.append(row[0])
--- rrd/view/portal/god.py
+++ rrd/view/portal/god.py
@@ -5,13 +5,16 @@ from rrd.service import god_sync_service
 from rrd import app
 from rrd.utils.logger import logging
 from rrd.model.portal.host import Host
+from rrd.model.portal.god_host import God_host
 log = logging.getLogger(__file__)
 
-from flask import jsonify,render_template
+from flask import jsonify,render_template,request
 
 class God():
-    def __init__(self,hostname,status):
+    def __init__(self,hostname,ip,ips,status):
         self.hostname = hostname
+        self.ip = ip
+        self.ips = ips
         self.status = status
 
 @app.route('/god/god_sync', methods=["GET"])
@@ -21,15 +24,25 @@ def god_sync():
 
 @app.route("/god/server_status")
 def server_info():
+    product = request.args.get('product','OPS')
     ret = []
-    dict = Host.all_host_dict()
-    for i in dict:
-        status = detect_status(dict[i])
-        ins = God(i,status)
-        ret.append(ins)
+    dict = Host.all_host_ip_dict()
+    product_host_dict = God_host.product_host_dict(product)
+    products = product_list()
+    for i in product_host_dict:
+        if i in dict:
+            status = detect_status(dict[i])
+            ins = God(i, dict[i], product_host_dict[i], status)
+            ret.append(ins)
+        else:
+            status = "not be installed"
+            ins = God(i, "", product_host_dict[i], status)
+            ret.append(ins)
+
     return render_template(
         'portal/god/index.html',
         data={
+            'products': products,
             'vs': ret,
         }
     )
@@ -44,4 +57,8 @@ def detect_status(ip):
     except Exception,e:
         log.error(e)
         return "dead"
-    return "active"
+    return "dead"
+
+def product_list():
+    product = God_host.all_product()
+    return product
